15b22c64b4db0c2adee8bdb37bf923c0b7b80532
Jaideep Bhoosreddy <jaideepb@buffalo.edu>
2016-10-06 22:10:06 -0400

    update init command

--- filesystem.js
+++ filesystem.js
@@ -1,9 +1,8 @@
 const { Task, exec } = require('./libs');
 const { MC_ROOT, DEBUGGER_ROOT } = require('./contants');
 const { log } = require('./server');
-const INIT_COMMAND = `cd ${DEBUGGER_ROOT} && git checkout mc-tracking && cd ..`;
-const UPDATE_COMMAND = `cd ${MC_ROOT} && hg pull -u && cd .. && cd ${DEBUGGER_ROOT} && git fetch upstream master && git reset --hard upstream/master
- && cd ..`;
+const INIT_COMMAND = `cd ${DEBUGGER_ROOT} && git checkout mc-tracking && git fetch upstream master && git reset --hard upstream/master && cd ..`;
+const UPDATE_COMMAND = `cd ${MC_ROOT} && hg pull -u && cd .. && cd ${DEBUGGER_ROOT} && git fetch upstream master && git reset --hard upstream/master && cd ..`;
 
 const init = Task.async(function*() {
   log('Initializing file system.');
@@ -13,7 +12,7 @@ const init = Task.async(function*() {
 
 const update = Task.async(function*() {
   log('Updating file system.');
-   return execute(UPDATE_COMMAND);
+  return execute(UPDATE_COMMAND);
 });
 
 const execute = Task.async(function*(command) {
